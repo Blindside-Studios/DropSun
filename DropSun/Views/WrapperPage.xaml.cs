540a9214b70a3144071be68c5d85a126b0c36092
Blindside Studios <64647061+Blindside-Studios@users.noreply.github.com>
2025-01-27 23:49:58 +0100

    Removed debug sidebar icons
    They are not needed anymore... currently.

--- DropSun/Views/WrapperPage.xaml.cs
+++ DropSun/Views/WrapperPage.xaml.cs
@@ -3,20 +3,9 @@ using DropSun.Model.Weather;
 using DropSun.Views.Controls;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Animation;
 using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -24,7 +13,7 @@ using Windows.Foundation.Collections;
 namespace DropSun.Views
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// Responsible for the split view with the sidebar and holding the sidebar items.
     /// </summary>
     public sealed partial class WrapperPage : Page
     {
@@ -54,14 +43,9 @@ namespace DropSun.Views
                 Temperature = 0,
                 Precipitation = 0,
             };
-            //weatherItem.Weather = new Weather()
-            //{
-            //    Forecast = null
-            //};
             LocationsListView.Items.Add(weatherItem);
 
             var weatherForecast = await OpenMeteoAPI.GetWeatherAsync(SelectedLocation.latitude, SelectedLocation.longitude);
-            //Debug.WriteLine(weatherForecast.ToString());
             weatherItem.Weather = weatherForecast;
             weatherItem.Temperature = (double)weatherForecast.Current.Temperature2M;
             weatherItem.Precipitation = (int)weatherForecast.Current.Precipitation;
@@ -144,47 +128,6 @@ namespace DropSun.Views
 
             currentAnimation = sb;
             sb.Begin();
-        }
-
-        private void ShowSunnyButton_Click(object sender, RoutedEventArgs e)
-        {
-            Model.ViewModels.WeatherState.Instance.Forecast = null;
-            FrameNavigationOptions navOptions = new FrameNavigationOptions();
-            navOptions.TransitionInfoOverride = new DrillInNavigationTransitionInfo();
-            Type pageType = typeof(WeatherView);
-            ContentFrame.NavigateToType(pageType, null, navOptions);
-        }
-
-        private void ShowRainButton_Click(object sender, RoutedEventArgs e)
-        {
-            Model.ViewModels.WeatherState.Instance.Forecast = null;
-            FrameNavigationOptions navOptions = new FrameNavigationOptions();
-            navOptions.TransitionInfoOverride = new DrillInNavigationTransitionInfo();
-            Type pageType = typeof(WeatherView);
-            ContentFrame.NavigateToType(pageType, null, navOptions);
-        }
-
-        private async void ConfirmCityButton_Click(object sender, RoutedEventArgs e)
-        {
-            //await Model.Weather.ObtainWeather.FromOpenMeteo(ExampleCityTextBox.Text);
-        }
-
-        private void LocationsListView_RightTapped(object sender, RightTappedRoutedEventArgs e)
-        {
-            if (ExampleCityTextBox.Visibility == Visibility.Collapsed)
-            {
-                ExampleCityTextBox.Visibility = Visibility.Visible;
-                ConfirmCityButton.Visibility = Visibility.Visible;
-                ShowSunnyButton.Visibility = Visibility.Visible;
-                ShowRainButton.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                ExampleCityTextBox.Visibility = Visibility.Collapsed;
-                ConfirmCityButton.Visibility = Visibility.Collapsed;
-                ShowSunnyButton.Visibility = Visibility.Collapsed;
-                ShowRainButton.Visibility = Visibility.Collapsed;
-            }
         }
     }
 }
