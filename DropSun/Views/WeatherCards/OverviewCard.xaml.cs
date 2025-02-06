dde18c5231ec0b78bf12343955b21a4091ff4935
Blindside Studios <64647061+Blindside-Studios@users.noreply.github.com>
2025-02-06 18:47:26 +0100

    Designed overview widget
    The overview widget is now displaying real data (other than UV and AQI, those are still hardcoded).

--- DropSun/Views/WeatherCards/OverviewCard.xaml.cs
+++ DropSun/Views/WeatherCards/OverviewCard.xaml.cs
@@ -15,6 +15,11 @@ using Windows.Foundation.Collections;
 using Windows.UI.Composition;
 using Microsoft.UI.Composition;
 using Microsoft.UI.Xaml.Hosting;
+using DropSun.Model.ViewModels;
+using Windows.ApplicationModel.Resources;
+using DropSun.Model.Geolocation;
+using DropSun.Model.Weather;
+using System.Diagnostics;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -34,7 +39,30 @@ namespace DropSun.Views.WeatherCards
 
         private void OverviewCard_Loaded(object sender, RoutedEventArgs e)
         {
+            WeatherState.Instance.PropertyChanged += Instance_PropertyChanged;
+            updateWeatherCards();
+        }
 
+        private void Instance_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            updateWeatherCards();
+        }
+
+        private void updateWeatherCards()
+        {
+            if (WeatherState.Instance.Forecast != null && WeatherState.Instance.Location != null)
+            {
+                ResourceLoader _loader = ResourceLoader.GetForViewIndependentUse();
+                ResourceLoader _conditionsLoader = ResourceLoader.GetForViewIndependentUse("Conditions");
+
+                CountryTextBlock.Text = GeoLookup.GetCountryName(WeatherState.Instance.Location.country_code);
+                TemperatureTextBlock.Text = $"{WeatherState.Instance.Forecast.Current.Temperature2M}{WeatherState.Instance.Forecast.CurrentUnits.Temperature2M[0]}";
+                string apparentTemperature = string.Format(_loader.GetString("ApparentTemperatureString"),
+                    WeatherState.Instance.Forecast.Current.ApparentTemperature,
+                    WeatherState.Instance.Forecast.CurrentUnits.ApparentTemperature[0].ToString());
+                ApparentTemperatureTextBlock.Text = apparentTemperature;
+                ConditionsTextBox.Text = _conditionsLoader.GetString(WeatherState.Instance.Forecast.GetWeatherDescription().ToString());
+            }
         }
     }
 }
