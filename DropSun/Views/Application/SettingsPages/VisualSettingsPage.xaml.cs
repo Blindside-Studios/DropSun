75e0d2dab5d2cc474c419623fb6c35cc52b9d76a
Blindside Studios <64647061+Blindside-Studios@users.noreply.github.com>
2025-03-17 00:11:18 +0100

    Sun Bounciness Settings implemented
    The sun can now be toggled between gentle and bouncy

--- DropSun/Views/Application/SettingsPages/VisualSettingsPage.xaml.cs
+++ DropSun/Views/Application/SettingsPages/VisualSettingsPage.xaml.cs
@@ -12,6 +12,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using DropSun.Model.ViewModels;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,6 +27,49 @@ namespace DropSun.Views.Application.SettingsPages
         public VisualSettingsPage()
         {
             this.InitializeComponent();
+            this.Loaded += VisualSettingsPage_Loaded;
+            AppSettings.Instance.PropertyChanged += Instance_PropertyChanged;
+        }
+
+        private void loadSettings()
+        {
+            switch (AppSettings.Instance.InteractionStyle)
+            {
+                case Pikouna_Engine.SunInteractionStyle.None:
+                    InteractionsRadioButtonGroup.SelectedIndex = 0;
+                    break;
+                case Pikouna_Engine.SunInteractionStyle.Gentle:
+                    InteractionsRadioButtonGroup.SelectedIndex = 1;
+                    break;
+                case Pikouna_Engine.SunInteractionStyle.Bouncy:
+                    InteractionsRadioButtonGroup.SelectedIndex = 2;
+                    break;
+            }
+        }
+
+        private void VisualSettingsPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            loadSettings();
+        }
+
+        private void Instance_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(AppSettings.Instance.InteractionStyle)) loadSettings();
+        }
+
+        private void OffRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            AppSettings.Instance.InteractionStyle = Pikouna_Engine.SunInteractionStyle.None;
+        }
+
+        private void SlowRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            AppSettings.Instance.InteractionStyle = Pikouna_Engine.SunInteractionStyle.Gentle;
+        }
+
+        private void BouncyRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            AppSettings.Instance.InteractionStyle = Pikouna_Engine.SunInteractionStyle.Bouncy;
         }
     }
 }
